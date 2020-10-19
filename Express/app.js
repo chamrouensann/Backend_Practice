e2c8478addb0fb7389dab2545ebfef3386f4d778
chamrouensann <chamrouensann.geek@gmail.com>
2020-10-19 11:11:23 +0700

    Parsing the JSON data

--- Express/app.js
+++ Express/app.js
@@ -1,17 +1,22 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
+// const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev/test.json`));
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev/test.json`));
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'ok',
+    result: tours.length,
+    data: { tours },
+  });
+});
+
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.status(200).send('Server express is running.');
-});
-
-app.post('/', (req, res) => {
-  res.status(404).send('This is post method');
-});
-
 app.listen(port, () => {
-  console.log(`The server is running on port ${port}`);
+  console.log(`The Application listing on port${port}`);
 });
